94954bed52682f6ce485968b16d19715dd020109
Xeno Kovah <xeno@ost.fyi>
2021-03-15 11:17:59 -0400

    Fixing return value

--- ShiftExample1/ShiftExample1.c
+++ ShiftExample1/ShiftExample1.c
@@ -3,7 +3,7 @@
 // the MPL was not distributed with this file, You can 
 // obtain one at https://mozilla.org/MPL/2.0/.
 
-void main()
+unsigned int main()
 {
     unsigned int a, b, c;
     a = 0x5;
